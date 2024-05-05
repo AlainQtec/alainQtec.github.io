3d5afe25931d61e1947da79a3ec2bb2712605584
alainQtec <alainQtec@gmail.com>
2024-05-05 22:34:09 +0300

    Update vite.config.ts

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -1,7 +1,6 @@
 import { staticAdapter } from "@builder.io/qwik-city/adapters/static/vite";
 import { extendConfig } from "@builder.io/qwik-city/vite";
 import baseConfig from "../../vite.config";
-
 import { SITE } from "../../src/config.mjs";
 
 export default extendConfig(baseConfig, () => {
